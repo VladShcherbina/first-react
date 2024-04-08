eb886885e679ea8e7e26addd0b0d69d0be1cf87b
Vlad Shcherbina <Shherbynavladik998@gmail.com>
2024-04-08 18:23:32 +0300

    first-react

---
+++ src/components/Center.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Center.css';
+
+
+class Center extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="center">Central block
+            </div>
+        )
+    }    
+
+}
+
+export default Center
---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Navigation.css';
+
+
+class Navigation extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="navigation">
+            Left side navigation</div>
+        )
+    }    
+
+}
+
+export default Navigation
